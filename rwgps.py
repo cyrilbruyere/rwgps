correct IF calculation

rwgps.py
@@ -233,7 +233,7 @@ df = df.groupby(['DATE']).sum().reset_index()
 
 df['SPEED'] = df['DISTANCE'] / df['DUREE']
 df['RATIO'] = df['ELEVATION'] / df['DISTANCE']
-df['IF'] = df['DUREE'] * df['SPEED'] / 27.5 * np.power(df['RATIO'], 1/3) / math.pow(22.5, 1/3)
+df['IF'] = df['SPEED'] / 27.5 * np.power(df['RATIO'], 1/3) / math.pow(22.5, 1/3)
 df['TSS'] = df['DUREE'] * np.power(df ['IF'], 2) * 100
 df['DATE'] = df['DATE'].astype(str)
 
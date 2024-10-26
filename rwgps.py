00a1d79949fbd681e68bb218139dac0b6332c38c
cyrilbruyere <cyril.bruyere@gmail.com>
2024-10-26 15:41:41 +0200

    add to_list to pass first row as header

--- rwgps.py
+++ rwgps.py
@@ -147,9 +147,8 @@ velotaf_stats = velotaf_stats.groupby(['YYYY']).agg({'DATE' : 'nunique', 'DISTAN
 velotaf_stats = velotaf_stats.astype(int)
 velotaf_stats.index = ['An', 'Jours', 'Kms']
 velotaf_stats = velotaf_stats.reset_index()
-velotaf_header = velotaf_stats.iloc[0].to_list()
+velotaf_stats.columns = velotaf_stats.iloc[0].to_list()
 velotaf_stats = velotaf_stats.iloc[1:]
-velotaf_stats.columns = velotaf_header
 
 rides_stats = trips[trips['GEAR'].isin(['ROAD', 'GRAVEL'])][['YYYY', 'DATE', 'DISTANCE', 'DUREE']]
 rides_stats = rides_stats.groupby(['YYYY']).agg({'DATE' : 'nunique', 'DISTANCE' : ['sum', 'mean'], 'DUREE' : 'sum'}).reset_index()
@@ -163,7 +162,7 @@ rides_stats[['KM MOY', 'SPEED']] = round(rides_stats[['KM MOY', 'SPEED']], 1)
 rides_stats = rides_stats.T
 rides_stats.index = ['An', 'Jours', 'Km', 'AvgKm', 'Km/h']
 rides_stats = rides_stats.reset_index()
-rides_stats.columns = rides_stats.iloc[0]
+rides_stats.columns = rides_stats.iloc[0].to_list()
 rides_stats = rides_stats.iloc[1:]
 
 total_stats = trips[['YYYY', 'DATE', 'DUREE']]
@@ -171,7 +170,7 @@ total_stats = total_stats.groupby(['YYYY']).agg({'DATE' : 'nunique', 'DUREE' : '
 total_stats = total_stats.astype(int)
 total_stats.index = ['An', 'Jours', 'Heures']
 total_stats = total_stats.reset_index()
-total_stats.columns = total_stats.iloc[0]
+total_stats.columns = total_stats.iloc[0].to_list()
 total_stats = total_stats.iloc[1:]
 
 semester_stats = trips[trips['DATE'] > dt.date.today() - relativedelta(months = 6)][['YYYY', 'MM', 'DATE', 'DUREE']]
@@ -191,7 +190,7 @@ semester_stats[['KM_AVG', 'RATIO']] = round(semester_stats[['KM_AVG', 'RATIO']],
 semester_stats = semester_stats.T
 semester_stats.index = ['An', 'Heures', 'AvgKm', 'Ratio']
 semester_stats = semester_stats.reset_index()
-semester_stats.columns = semester_stats.iloc[0]
+semester_stats.columns = semester_stats.iloc[0].to_list()
 semester_stats = semester_stats.iloc[1:]
 
 # SUMMARY YTD

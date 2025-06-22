42b3c319d3b66d3f3cdc3cd371a1d0f00cc4039e
Cyril Bruyere <cyril.bruyere@volvo.com>
2025-06-22 20:01:15 +0200

    mismatch corrected

--- rwgps.py
+++ rwgps.py
@@ -313,7 +313,7 @@ pmc = pmc[pmc['DATE'] > rolling_months]
 # Avec Matplotlib
 ax = pmc.plot.area(x = 'DATE', y = ['TSB-', 'TSB+'], stacked = False, color = ['r', 'g'])
 ax = pmc.plot.line(x = 'DATE', y = ['CTL'], color = 'b', linewidth = 2)
-ax.set_xticks(ax.get_sticks(), ax.get_xticklabels(), rotation = 90, ha = 'right')
+ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation = 90, ha = 'right')
 fig = ax.get_figure()
 fig.savefig("pmc.png")
 

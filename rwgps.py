07727dbd090436938c39457ca3ec6ab3b9940db5
Cyril Bruyere <cyril.bruyere@volvo.com>
2025-06-23 09:03:21 +0200

    adjust xaxis

--- rwgps.py
+++ rwgps.py
@@ -311,10 +311,10 @@ pmc = pmc[pmc['DATE'] > rolling_months]
 # graf.write_image('pmc.png')
 
 # Avec Matplotlib
-# plt.area(y = pmc['TSB-', 'TSB+'].values, stacked = False, color = ['r', 'g'])
 plt.plot(pmc['DATE'].values, pmc['CTL'].values, color = 'b', linewidth = 2)
-plt.fill_between(pmc['DATE'].values, pmc['TSB-'].values, color = 'r')
-plt.fill_between(pmc['DATE'].values, pmc['TSB+'].values, color = 'g')
+plt.fill_between(pmc['DATE'].str[-5:].values, pmc['TSB-'].values, color = 'r')
+plt.fill_between(pmc['DATE'].str[-5:].values, pmc['TSB+'].values, color = 'g')
+plt.xticks(rotation = 90)
 plt.savefig("pmc.png")
 
 # Images à envoyer
